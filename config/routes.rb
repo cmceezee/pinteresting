e62ee3b85376259291c7745343b06ed533939384
Chris D. McCoy <chrisdmccoy@gmail.com>
2013-12-22 13:50:07 -0800

    adding bootstrap gem and why page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" # creates about_path
+  get "why" => "pages#why" # creates why_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
