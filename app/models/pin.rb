e818cb29fcb3ce8849a8dda095ac8c50b4e3819d
Chris D. McCoy <chrisdmccoy@gmail.com>
2014-04-17 20:31:40 -0700

    adding large image size

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :large =>"600x600>" }
 
 	  validates :description, presence: true
 	  validates :image, presence: true
